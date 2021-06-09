1092ad35b796a05fba27b6207cedf6e5a310db03
bw119 <bw119@lab-rg07-34.cms.waikato.ac.nz>
2021-06-09 16:59:33 +1200

    Added REsearch file

---
+++ REsearch.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class REsearch {
+
+    public static void main(String[] args) {
+        String filename = args[0];
+
+        try {
+            //BufferedReader inputFile = new BufferedReader(new InputStreamReader(System.in));
+            //BufferedReader inputFSM = new BufferedReader(new InputStreamReader(System.in));
+
+        }
+
+        catch (Exception e) {
+            System.out.println("Invalid piped in file");
+            e.printStackTrace();
+        }
+    }
+}
