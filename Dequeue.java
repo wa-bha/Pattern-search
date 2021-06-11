f01ab4743a062bfa8f117218049b62c5b82be6cd
bw119 <bw119@lab-rg06-02.cms.waikato.ac.nz>
2021-06-11 17:50:42 +1200

    Added Deque structure

---
+++ Dequeue.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+// Double-ended queue implementation w/ Restricted output deque
+
+public class Dequeue {
+    private List<Integer> deque = new ArrayList<Integer>();
+
+    public Dequeue() {
+        //Initialize the queue with the scan state
+    }
+
+    // Push the next item onto the HEAD?
+    public void push() {
+
+    }
+
+    // Put the next item into the TAIL?
+    public int put() {
+        return 1;
+    }
+
+    // Pop the next item in the head
+    public void pop() {
+
+    }
+}
