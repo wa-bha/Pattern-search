6ee695c3444bb871b41c4342ca92a5c33da63647
bw119 <bw119@lab-rg06-35.cms.waikato.ac.nz>
2021-06-12 23:21:32 +1200

    Implemented pattern matching method attempt

--- REsearch.java
+++ REsearch.java
@@ -3,16 +3,19 @@ import java.util.*;
 
 public class REsearch {
 
-    //Storing FSM
-    static ArrayList<String> ch = new ArrayList<String>();  
-    static ArrayList<Integer> next1 = new ArrayList<Integer>();
-    static ArrayList<Integer> next2 = new ArrayList<Integer>();
+    //Storing states from the FSM
+    static ArrayList<stateNode> allStates = new ArrayList<stateNode>();
+    static Integer pointer = 0;
+    static Integer marker = 0;
+    static Deque deque;
+    static Integer SCAN_STATE = -5;
+    static Boolean foundMatch = false;
 
     public static void main(String[] args) {
         //Check correct usage and standard input
         if (args.length == 0) {
             System.err.println("Please enter a valid input text file");
-            System.err.println("Usage example: testFSM.txt | java Dequeue MobyDick.txt\n");
+            System.err.println("Usage testing example: cat brownTestFSM.txt | java Dequeue brownTestInput.txt > out.txt\n");
             System.exit(0);
         }
         getFSM();
@@ -20,23 +23,28 @@ public class REsearch {
         //DELETE HERE
         testPrintFSM();
 
-        //Create dequeue and initialize "SCAN"
-        //Deque deque = new Deque();
-
         // Get the file into the buffered reader and read next line
         String nextLine;
         String filename = args[0];
+
+        //DEBUG FILEREADER
+        //try (BufferedReader fileReader = new BufferedReader(new FileReader("brownTestInput.txt"))){
+
         try (BufferedReader fileReader = new BufferedReader(new FileReader(filename))){
             nextLine = fileReader.readLine();
             
-            //While there are still lines in the file
-
             while (nextLine != null) {
-                //If a matching pattern is found in the line ........
-                if (executePatternSearch(nextLine)) {
-                    System.out.println("Match found!!!");
-                }
 
+                String[] splitChars = nextLine.split("");
+                executePatternSearch(splitChars);
+                System.err.println(foundMatch);
+
+                if (foundMatch) {
+                    System.err.println("Match found!!!");
+                    System.err.println(nextLine);
+                    System.out.println(nextLine);
+                    foundMatch = false;
+                }
                 nextLine = fileReader.readLine();
             }
 
@@ -46,48 +54,99 @@ public class REsearch {
         }
     }
 
-    // Recieves a string containing line from text file
+    // Recieves a string array containing split line from text file
     // Checks a substring can go through FSM and reach the final state -> contains pattern)
-    public static boolean executePatternSearch(String line) {
-        return true;
-        //Check state character if branch
-        //move to next state
+    public static void executePatternSearch(String[] splitLine) {
+        try {
+            //While we have not reached the end of the 
+            while (marker <= splitLine.length) {
+                //Create dequeue and initialize "SCAN", "START STATE"
+                deque = new Deque();
+                deque.addHead(SCAN_STATE);
+                deque.addHead(0);
+                
+                // Get the next head()
+                int currentStateValue = deque.getHead();
 
-        //If not brnach
-        //match character
-        //if the character matches, -
-        // 1. move to next character in line
-        // 2. move to next state in FSM as current state has matched
-        // 3. match character again (loop)
+                System.err.println(currentStateValue);
 
-        //if character not matched, shift marker to try next character set
-        
-        // Same loop again until final state in FSM (null)
-        
-        
-    }
+                // While the head is not the "SCAN" state
+                while (currentStateValue != SCAN_STATE) {
+                    stateNode currentNode = allStates.get(currentStateValue);
 
-    public static void testPrintFSM() {
-        System.out.println("s\tch\tn1\tn2");
-        System.out.println("--------------------------");
-        for(int i = 0; i < ch.size(); i++) {
-        System.out.println(i + "\t" + ch.get(i) + "\t" + next1.get(i) + "\t" + next2.get(i));
+                    //If it is a BRANCH state
+                    if (currentNode.ch == "br") {
+
+                        //If it is the start state
+                        if (currentNode.next1 == currentNode.next2) {
+                            deque.addTail(currentNode.next1);
+                            deque.getHead();
+
+                        }
+                        //Else, add both .next values to deque
+                        deque.addTail(currentNode.next1);
+                        deque.addTail(currentNode.next2);
+                        deque.getHead();
+                    }
+
+                    //Otherwise it is a CHARACTER state - check CHAR == INPUT
+                    if (currentNode.ch == splitLine[pointer]) {
+
+                        //If the next state is the end of the FSM (char == null)
+                        if (allStates.get(currentNode.next1).ch == null) {
+                            foundMatch = true;
+                        }
+                        pointer++;
+                        deque.addTail(currentNode.next1);
+                        deque.getHead();
+                    }
+
+                    //Else, pattern will not match - try next character set
+                    else {
+                        //Increment primary pointer and reset secondary
+                        marker++;
+                        pointer = marker;
+                    }
+                }
+                if (deque.head.next != null) {
+                    deque.getHead();
+                    deque.addTail(SCAN_STATE);
+                }
+            }
+
+        } catch (Exception e) {
+            System.err.println("Invalid file");
+            e.printStackTrace();
         }
     }
 
-    //Read standard input to populate the FSM arrays
+    // Test print the FSM
+    public static void testPrintFSM() {
+        System.err.println("s\tch\tn1\tn2");
+        System.err.println("--------------------------");
+        int nodeCount = 0;
+        for (stateNode stateNode : allStates) {
+            System.err.println(nodeCount + "\t" + stateNode.ch + "\t" + stateNode.next1 + "\t" + stateNode.next2);
+            nodeCount++;
+        }
+        System.err.println();
+    }
+
+    // Read standard input to populate the FSM arrays
     public static void getFSM() {
 
         String line = " ";
         String[] split;
 
+        //DEBUG FILEREADER
+        //try (BufferedReader fileReader = new BufferedReader(new FileReader("brownTestInput.txt"))){
+
+
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
             line = reader.readLine();
             while (line != null) {
                 split = line.split(",");
-                ch.add(split[0]);
-                next1.add(Integer.parseInt(split[1]));
-                next2.add(Integer.parseInt(split[2]));
+                allStates.add(new stateNode(split[0].toString(), Integer.parseInt(split[1]), Integer.parseInt(split[2])));
                 line = reader.readLine();
             }
         } catch(Exception e) {
---
+++ stateNode.java
@@ -0,0 +1,11 @@
+public class stateNode {
+    public String ch;
+    public Integer next1;
+    public Integer next2;
+
+    public stateNode(String ch_, Integer next1_, Integer next2_) {
+        this.ch = ch_;
+        this.next1 = next1_;
+        this.next2 = next2_;
+    }
+}
