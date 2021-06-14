9e5947fad1a3d19a075bf405d1d8c483906ca3fb
wa-bha <55519189+wa-bha@users.noreply.github.com>
2021-06-14 23:18:37 +1200

    Fixed Pattern matching algorithm structure and updated deque

--- Deque.java
+++ Deque.java
@@ -37,18 +37,25 @@ public class Deque {
     }
 
     // Pop the next item from the head and return it- get()
-    public Integer getHead() throws Exception {
-        if (head == null) {
-            throw new Exception();
-        }
+    public Integer getHead() {
         dNode currentHead = head;
         head = head.next;
         return currentHead.stateNumber;
     }
 
-    // Testing method to view current state of HEADSET DIED :( I can hear you
+    public Boolean checkHead() {
+        if (head == null) {
+            return false;
+        }
+        return true;
+    } 
+
+    // Testing method to view current state of deque
     public void print() {
         dNode current = head;
+        System.err.println();
+        System.err.println("Current States in Deque:");
+        System.err.println("----------------------------");
         while (current != null) {
             // Print out the head client's arrival and use time in terminal
             System.err.println("Head" + "\t" + "Tail" + "\t" + "State Number");
